d28e93ea93554ee228792976329f54bd07e73ae5
Robert Thompson <rthomps7@nd.edu>
2015-03-31 08:47:19 -0400

    Initial commit

---
+++ example/main.js
@@ -0,0 +1,26 @@
+
+require.config({
+  baseUrl: '/',
+  paths: {
+    css:           'bower_components/require-css/css.min',
+    'css-builder': 'bower_components/require-css/css-builder',
+    normalize:     'bower_components/require-css/normalize',
+    ractive:       'bower_components/ractive/ractive',
+    rv:            'bower_components/rv/rv',
+    pikaday:       'bower_components/pikaday/pikaday'
+  },
+  shim: {
+    'lib/main': ['ractive']
+  }
+});
+
+//requirejs(['ractive', 'rv!example/template', 'dist/pikaday.min'],
+requirejs(['ractive', 'rv!example/template', 'lib/main'],
+function(Ractive, template) {
+  var ractive = new Ractive({
+        el: '#container',
+        template: template,
+        data: {
+        }
+      });
+});
